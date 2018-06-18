d936357a64f6d5771d39678f95135c0914faecaa
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 21:49:56 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
       language.each do |name, attributes|
-        
+        new_hash ||=
       end
   end
 end
