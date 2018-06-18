a7419fb00a44f47ef59bd6d9817824212d6c4f3c
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 22:03:39 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,22 +1,14 @@
 def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |category, languages|
-    languages.each do |language, attributes|
-      attributes.each do |attribute, value|
-        if language == :javascript
-          new_hash[language] = {
-            :type => value,
-            :style => [:oo, :functional]
-          }
-        else
-          new_hash[language] = {
-            :type => value,
-            :style => [category]
-          }
+ new_hash = {}
+  languages.each do |style,language|
+    language.each do |name,type|
+        if new_hash[name] == nil
+          new_hash[name] = type
+          new_hash[name][:style] = []
         end
-          
-      end
+        new_hash[name][:style] << style
     end
   end
+  new_hash
  
 end
